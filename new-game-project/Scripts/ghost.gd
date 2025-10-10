f48c16a8f722e4c65bb41a18b519867483f49636
MJMrigg <riggmatthew20@outlook.com>
2025-10-10 19:05:07 -0400

    Start Possession
    Work Done:
    - Added Possession Mechanic. Still has problem of mesh doesn't change.
    
    Todo:
    - Finish Possession Mechanic

--- new-game-project/Scripts/ghost.gd
+++ new-game-project/Scripts/ghost.gd
@@ -1,6 +1,13 @@
 extends CharacterBody3D
 
-@export var speed = 5.0
+@export var speed:float = 5.0 # Movement speed
+@export var possesed:Node3D # Target that is possesed
+var possessor:ShapeCast3D # Detector for possession area
+var mesh:MeshInstance3D # Player's mesh
+
+func _ready() -> void:
+	possessor = get_node("PossessionArea")
+	mesh = get_node("MeshInstance3D")
 
 func _process(delta: float) -> void:
 	# Get translation and rotation
@@ -15,3 +22,26 @@ func _process(delta: float) -> void:
 	
 	# MOVE
 	move_and_slide()
+	
+	# If the player tries to posses someone
+	if(Input.is_action_just_pressed("p_interact")):
+		if(possesed == null):
+			print("TEST")
+			posses()
+	
+	# If the player was possessing anyone, move them the player position
+	if(possesed != null):
+		possesed.position = self.position
+
+func posses() -> void:
+	# If the player is already possesing someone, return
+	if(possesed != null):
+		return
+	
+	# Get closest NPC to player
+	possessor.force_shapecast_update()
+	possesed = possessor.get_collider(0) # No checks needed, since possessor only looks for NPCs
+		
+	# Make the NPC invisible and the player's mesh the npc's mesh so that it looks like the player possessed them
+	possesed.hide()
+	mesh.mesh = possesed.
