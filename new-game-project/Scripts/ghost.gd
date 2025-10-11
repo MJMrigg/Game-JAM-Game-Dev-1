c48236441fb6dee02a02482a5151af38399924ff
MJMrigg <riggmatthew20@outlook.com>
2025-10-11 15:35:31 -0400

    Fixed Possession on Main
    Work Done:
    - Fixed possession issue on Main Scene
    - Added mouse control to player rotation
    
    Todo:
    - Implement animations
    - Implement menu

--- new-game-project/Scripts/ghost.gd
+++ new-game-project/Scripts/ghost.gd
@@ -1,37 +1,49 @@
 extends CharacterBody3D
 
 @export var speed:float = 5.0 # Movement speed
+@export var mouse_sens:float = 0.1 # Mouse sensative speed
 @export var possesed:Node3D # Target that is possesed
 var possessor:ShapeCast3D # Detector for possession area
 var collider:CollisionShape3D # Collision detector
 var sound_area:Area3D # Sound Area
+var interactor:ShapeCast3D # Detector for objects to interact with
+var knife:bool = false # If the player has the knife
+signal wrong_door
 
 func _ready() -> void:
 	possessor = get_node("PossessionArea")
 	collider = get_node("CollisionShape3D")
 	sound_area = get_node("SoundArea3D")
+	interactor = get_node("Interactor")
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED) # Locks mouse in window
+
+# Have mouse rotate camera
+func _input(event: InputEvent) -> void:
+	# For each Input it checks if it was a mouse movement. If yes..
+	if event is InputEventMouseMotion:
+		rotate_y(deg_to_rad(-event.relative.x * mouse_sens)) # rotate cam along its Y (left to right)
 
 func _process(delta: float) -> void:
 	# Get translation and rotation
-	var translation = Input.get_axis("p_forward", "p_backward")
-	var rotation = Input.get_axis("p_right", "p_left")
-	
-	# Rotate around the y axis
-	rotate_y(rotation*delta*(PI/2))
+	var forward = Input.get_axis("p_forward", "p_backward")
+	var right = Input.get_axis("p_right", "p_left")
 	
 	# Set up velocity
-	velocity = transform.basis.z * translation * speed
+	velocity = (transform.basis.z * forward * speed) + (transform.basis.x * right * speed * -1)
 	
 	# MOVE
 	move_and_slide()
 	
 	# If the player tries to posses someone
-	if(Input.is_action_just_pressed("p_interact")):
+	if(Input.is_action_just_pressed("p_posses")):
 		if(possesed == null):
 			posses()
 		elif(possesed != null):
 			unposses()
-			
+	# If the player tries to interact with an object while possesing someone
+	if(Input.is_action_just_pressed("p_interact") && possesed != null):
+		interact()
+	
 	'''
 	# Alternet sound check - David T
 	if(Input.is_action_just_pressed("p_sound")):
@@ -55,6 +67,7 @@ func posses() -> void:
 	if(found == null): # If there was no NPC, return
 		return
 	possesed = found as NPC
+	print(found)
 	# If the NPC is unconcious, do not posses them
 	if(possesed.unconcious):
 		possesed = null
@@ -120,3 +133,24 @@ func earshot_exited(body: Node3D) -> void:
 	#return
 	if(body.is_in_group("NPCs")):
 		body.earshot = false
+
+# Interact with an object
+func interact():
+	# If the player isn't possesing anyone, do nothing
+	if(possesed == null):
+		return
+	
+	# Get closest object for the player to interact with
+	var found = interactor.get_collider(0)
+	if(found == null):
+		return
+	elif(found is door):
+		if(possesed.my_door == found):
+			found.queue_free()
+		else:
+			wrong_door.emit()
+			pass
+	elif(found is knife): # If it's the knife, make the knife show on all the models
+		found.queue_free()
+		get_node("NPC1/blade").show()
+		knife = true # The player now has the knife
