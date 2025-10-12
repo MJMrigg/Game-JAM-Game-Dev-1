7ef465bbf409b626adfa879d41f9ee57a889c84d
MJMrigg <riggmatthew20@outlook.com>
2025-10-12 15:50:11 -0400

    Tutorial DONE!

--- new-game-project/Scripts/menu.gd
+++ new-game-project/Scripts/menu.gd
@@ -1,5 +1,9 @@
 extends Control
 
-# Change scenes to main game when play button pressed
+# Change scene to main game when play button pressed
 func play() -> void:
 	get_tree().change_scene_to_file("res://Scenes/Morgan_Creek.tscn")
+
+# Change scene to tutorial when tutorial button is pressed
+func tutorial() -> void:
+	get_tree().change_scene_to_file("res://Scenes/tutorial_1.tscn")
--- new-game-project/Scripts/tutorial.gd
+++ new-game-project/Scripts/tutorial.gd
@@ -6,10 +6,14 @@ func next() -> void:
 	if(scene == 1):
 		get_tree().change_scene_to_file("res://Scenes/tutorial_2.tscn")
 	elif(scene == 2):
+		get_tree().change_scene_to_file("res://Scenes/tutorial_3.tscn")
+	elif(scene == 3):
 		get_tree().change_scene_to_file("res://Scenes/menu.tscn")
 
 func prev() -> void:
-	if(scene == 2):
-		get_tree().change_scene_to_file("res://Scenes/tutorial_1.tscn")
+	if(scene == 3):
+		get_tree().change_scene_to_file("res://Scenes/tutorial_2.tscn")
+	elif(scene == 2):
+		get_tree().change_scene_to_file("res://Scenes/tutorial_2.tscn")
 	elif(scene == 1):
 		get_tree().change_scene_to_file("res://Scenes/menu.tscn")
