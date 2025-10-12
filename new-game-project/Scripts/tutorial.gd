1512a6fd735bbd097c017b533abc8f37e3ef2290
MJMrigg <riggmatthew20@outlook.com>
2025-10-12 16:14:40 -0400

    Fixedd Menu issue

--- new-game-project/Scripts/tutorial.gd
+++ new-game-project/Scripts/tutorial.gd
@@ -14,6 +14,6 @@ func prev() -> void:
 	if(scene == 3):
 		get_tree().change_scene_to_file("res://Scenes/tutorial_2.tscn")
 	elif(scene == 2):
-		get_tree().change_scene_to_file("res://Scenes/tutorial_2.tscn")
+		get_tree().change_scene_to_file("res://Scenes/tutorial_1.tscn")
 	elif(scene == 1):
 		get_tree().change_scene_to_file("res://Scenes/menu.tscn")
