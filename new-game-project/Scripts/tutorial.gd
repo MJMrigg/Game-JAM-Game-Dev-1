e11999f934b3bd5e68fef9fea3be7f1ce2498067
MJMrigg <riggmatthew20@outlook.com>
2025-10-12 15:20:40 -0400

    Tutorials and score

---
+++ new-game-project/Scripts/tutorial.gd
@@ -0,0 +1,15 @@
+extends Control
+
+@export var scene:int
+
+func next() -> void:
+	if(scene == 1):
+		get_tree().change_scene_to_file("res://Scenes/tutorial_2.tscn")
+	elif(scene == 2):
+		get_tree().change_scene_to_file("res://Scenes/menu.tscn")
+
+func prev() -> void:
+	if(scene == 2):
+		get_tree().change_scene_to_file("res://Scenes/tutorial_1.tscn")
+	elif(scene == 1):
+		get_tree().change_scene_to_file("res://Scenes/menu.tscn")
